2b29eee02fdc605aaf0728c6dba25ed41ca5f417
Diablo94 <chauquejonathanemmanuel@gmail.com>
2023-09-20 18:49:14 -0300

    mejorando apariencia para agregar movimientos

--- agregar.php
+++ agregar.php
@@ -4,45 +4,63 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   <title>Nuevo movimiento</title>
+  <style>
+    /*  div {
+      background-color: blue;
+      color: white;
+      font-size: 50px;
+      margin: 20px;
+    } */
+  </style>
 </head>
 
 <body>
-  <h1>AGREGAR NUEVO MOVIMIENTO</h1>
+
   <?php
-    include 'conexion_db.php';
-    $resultado = mysqli_query($connection,"SELECT `id_familia`,`nombre` FROM `familiares`");
-    if (!$resultado) {
-      die("Error en la consulta: " . mysqli_error($connection));
-    }
+  include 'conexion_db.php';
+  $resultado = mysqli_query($connection, "SELECT `id_familia`,`nombre` FROM `familiares`");
+  if (!$resultado) {
+    die("Error en la consulta: " . mysqli_error($connection));
+  }
   ?>
 
-  <form action="insertar.php" method="post">
-    <label>Fecha: </label> <input type="date" name="fecham" id="fecham" ><br>
-    <label>Usuario: </label>
-    <select id="nombrem" name="nombrem">
+
+  <div class="container">
+    <h1>AGREGAR NUEVO MOVIMIENTO</h1>
+
+    <div class="row">
+      <form action="insertar.php" method="post">
+        <label>Fecha: </label> <input type="date" name="fecham" id="fecham"><br>
+        <label>Usuario: </label>
+        <select id="nombrem" name="nombrem" class="form-select" aria-label="Default select example">
           <?php while ($row = mysqli_fetch_assoc($resultado)) { ?>
-          <option value="<?php echo $row["id_familia"]. $row["nombre"] ; ?>">
-            <?php echo $row["id_familia"] . '-'. $row["nombre"]; ?>
-          </option>
+            <option value="<?php echo $row["id_familia"] . $row["nombre"]; ?>">
+              <?php echo $row["id_familia"] . '-' . $row["nombre"]; ?>
+            </option>
           <?php } ?>
-    </select>
-    <label>Tipo: </label>
-    <select id="tipom" name="tipom">
-      <option value="egreso">egreso</option>
-      <option value="ingreso">ingreso</option>
-    </select><br>
-    <label>Descripción:</label><input type="text" name="descripcionm" id="descripcionm" ><br>
-    <label>Monto: </label> <input type="float" name="montom" id="montom"><br>
-    <label>Forma: </label>
-    <select id="formam" name="formam">
-      <option value="transferencia bancaria">transferencia bancaria</option>
-      <option value="tarjeta de crédito" >tarejta de crédito</option>
-      <option value="efectivo" >efectivo</option>
-    </select><br>
-    <button type="submit">Agregar</button>
-    <button onclick="window.location.href='index.php'">Volver</button>
-  </form>
+        </select><br>
+        <label>Tipo: </label>
+        <select id="tipom" name="tipom" class="form-select" aria-label="Default select example">
+          <option value="egreso">egreso</option>
+          <option value="ingreso">ingreso</option>
+        </select>
+        <label>Descripción:</label><input type="text" name="descripcionm" id="descripcionm" class="form-control" id="formGroupExampleInput" placeholder="Descripción del movimiento"><br>
+        <label>Monto: </label> <input type="float" name="montom" id="montom" class="form-control" id="formGroupExampleInput" placeholder="$125.056,20"><br>
+        <label>Forma: </label>
+        <select id="formam" name="formam" class="form-select" aria-label="Default select example">
+          <option value="transferencia bancaria">transferencia bancaria</option>
+          <option value="tarjeta de crédito">tarejta de crédito</option>
+          <option value="efectivo">efectivo</option>
+        </select><br>
+        <button type="submit" class="btn btn-primary">Agregar</button>
+      </form>
+    </div>
+
+  </div>
 
 </body>
 
